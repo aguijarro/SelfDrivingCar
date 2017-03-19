51a673865c26f0f023d37665504ae5554e748047
Andres Guijarro <aguijarro@vic-data.com>
2017-03-18 23:01:58 -0500

    feat: Add dir_threshold function

--- AdvancedLaneFinding/calibration_camera.py
+++ AdvancedLaneFinding/calibration_camera.py
@@ -5,6 +5,30 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 
+
+# Define a function that applies Sobel x and y,
+# then computes the direction of the gradient
+# and applies a threshold.
+def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
+
+    # Apply the following steps to img
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # 2) Take the gradient in x and y separately
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # 3) Take the absolute value of the x and y gradients
+    abs_sobelx = np.absolute(sobelx)
+    abs_sobely = np.absolute(sobely)
+    # 4) Use np.arctan2(abs_sobely, abs_sobelx) to calculate the direction of the gradient
+    absgraddir = np.arctan2(abs_sobely, abs_sobelx)
+    # 5) Create a binary mask where direction thresholds are met
+    binary_output = np.zeros_like(absgraddir)
+    binary_output[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
+    # 6) Return this mask as your binary_output image
+    return binary_output
+
+
 # Define a function that applies Sobel x and y,
 # then computes the magnitude of the gradient
 # and applies a threshold
@@ -190,6 +214,8 @@ def main():
     mag_binary = mag_thresh(image, sobel_kernel=3, mag_thresh=(30, 100))
     draw_images(image, mag_binary, 'Thresholded Magnitude', 'gray')
 
+    dir_binary = dir_threshold(image, sobel_kernel=15, thresh=(0.7, 1.3))
+    draw_images(image, dir_binary, 'Thresholded Grad. Dir.', 'gray')
 
 if __name__ == '__main__':
     main()
