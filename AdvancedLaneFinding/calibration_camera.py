7194003f27d91268d0414b20d10cb7b0d1f3c9e7
Andres Guijarro <aguijarro@vic-data.com>
2017-03-19 00:07:40 -0500

    feat: Add pipeline function

--- AdvancedLaneFinding/calibration_camera.py
+++ AdvancedLaneFinding/calibration_camera.py
@@ -5,6 +5,32 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 
+
+def pipeline(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
+    img = np.copy(img)
+    # Convert to HSV color space and separate the V channel
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HLS).astype(np.float)
+    l_channel = hsv[:,:,1]
+    s_channel = hsv[:,:,2]
+    # Sobel x
+    sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0) # Take the derivative in x
+    abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
+    scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+
+    # Threshold x gradient
+    sxbinary = np.zeros_like(scaled_sobel)
+    sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= sx_thresh[1])] = 1
+
+    # Threshold color channel
+    s_binary = np.zeros_like(s_channel)
+    s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+    # Stack each channel
+    # Note color_binary[:, :, 0] is all 0s, effectively an all black image. It might
+    # be beneficial to replace this channel with something else.
+    color_binary = np.dstack(( np.zeros_like(sxbinary), sxbinary, s_binary))
+    return color_binary
+
+
 # Define a function that thresholds the S-channel of HLS
 # Use exclusive lower bound (>) and inclusive upper (<=)
 def hls_select(image, thresh=(0, 255)):
@@ -245,6 +271,10 @@ def main():
 
     hls_binary = hls_select(image6, thresh=(0, 255))
     draw_images(image6, hls_binary, 'Thresholded S', 'gray')
+
+    pipeline_img = pipeline(image)
+    draw_images(image6, pipeline_img, 'Pipeline Result', None)
+
 
 
 
