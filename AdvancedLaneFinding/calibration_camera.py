9e27b9e8fe065f2a7d3c6dbd055aadcd0afb276c
Andres Guijarro <aguijarro@vic-data.com>
2017-03-18 22:40:37 -0500

    feat: Add mag_thresh function

--- AdvancedLaneFinding/calibration_camera.py
+++ AdvancedLaneFinding/calibration_camera.py
@@ -5,6 +5,28 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 
+# Define a function that applies Sobel x and y,
+# then computes the magnitude of the gradient
+# and applies a threshold
+def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+    # Apply the following steps to img
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # 2) Take the gradient in x and y separately
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # 3) Calculate the magnitude
+    gradmag = np.sqrt(sobelx**2 + sobely**2)
+    # 4) Scale to 8-bit (0 - 255) and convert to type = np.uint8
+    scale_factor = np.max(gradmag)/255
+    gradmag = (gradmag/scale_factor).astype(np.uint8)
+    # 5) Create a binary mask where mag thresholds are met
+    binary_output = np.zeros_like(gradmag)
+    binary_output[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 1
+    # 6) Return this mask as your binary_output image
+    return binary_output
+
+
 # Define a function that applies Sobel x or y,
 # then takes an absolute value and applies a threshold.
 # Note: calling your function with orient='x', thresh_min=5, thresh_max=100
@@ -17,7 +39,7 @@ def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
     # 2) Take the derivative in x or y given orient = 'x' or 'y'
     if orient == 'x':
         sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
-    else:
+    if orient == 'y':
         sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
     # 3) Take the absolute value of the derivative or gradient
     abs_sobel = np.absolute(sobel)
@@ -68,12 +90,15 @@ def corners_unwarp(img, nx, ny, undistorted):
     return warped, M
 
 
-def draw_images(img, undistorted, title):
+def draw_images(img, undistorted, title, cmap):
     f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
     f.tight_layout()
     ax1.imshow(img)
     ax1.set_title('Original Image', fontsize=50)
-    ax2.imshow(undistorted)
+    if cmap is not None:
+        ax2.imshow(undistorted, cmap=cmap)
+    else:
+        ax2.imshow(undistorted)
     ax2.set_title(title, fontsize=50)
     plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
     plt.show()
@@ -154,11 +179,16 @@ def main():
     ##top_down, perspective_M = corners_unwarp(img, nx, ny, undistorted)
     ##draw_images(img, top_down, 'Undistorted and Warped Image')
 
+    # Run the function abs_sobel_thresh
+    grad_binary = abs_sobel_thresh(image,
+                                   orient='y',
+                                   thresh_min=20,
+                                   thresh_max=100)
+    draw_images(image, grad_binary, 'Thresholded Gradient', None)
 
     # Run the function
-    grad_binary = abs_sobel_thresh(image, orient='y', thresh_min=20, thresh_max=100)
-    draw_images(image, grad_binary, 'Thresholded Gradient')
-
+    mag_binary = mag_thresh(image, sobel_kernel=3, mag_thresh=(30, 100))
+    draw_images(image, mag_binary, 'Thresholded Magnitude', 'gray')
 
 
 if __name__ == '__main__':
